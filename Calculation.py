90a70619f59afe5eb06f004e06212b3b2734c14d
1305chirag <102894844+1305chirag@users.noreply.github.com>
2022-04-03 12:27:28 +0530

    Add files via upload

---
+++ Calculation.py
@@ -0,0 +1,9 @@
+class cal():
+    def add(self,a,b):
+        print(a+b)
+    def sub(self,a,b):
+        print(a-b)
+    def mul(self,a,b):
+        print(a*b)
+    def div(self,a,b):
+        print(a/b)
